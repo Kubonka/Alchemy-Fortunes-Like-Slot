8edea6f22ef6348ea362a6a23ce88fa3b457a0a0
Alberto Martinez <srbetito85@gmail.com>
2023-08-13 18:22:36 -0300

    first

---
+++ src/helperClasses/GameState.js
@@ -0,0 +1,7 @@
+const GameState = Object.freeze({
+  Initialize: Symbol("Initialize"),
+  NewGame: Symbol("NewGame"),
+  Loading: Symbol("Loading"),
+  Animating: Symbol("Animating"),
+});
+export default GameState;
